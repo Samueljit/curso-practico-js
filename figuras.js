0d97292e57ffe109f3cc31d06abd05a97b21db44
Samuel Infante <samueljit@gmail.com>
2021-11-30 23:21:33 -0300

    Termino de programa sobre calcular el area y perimetro de una figura falta mejorar el html con CSS

--- figuras.js
+++ figuras.js
@@ -46,3 +46,40 @@ function areaCirculo(radio){
 }
 
 console.groupEnd();
+
+//Triagulo isósceles.//
+
+function alturaTrianguloIsosceles(trianguloGrandeLado1, trianguloGrandeLado2, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLado1 != trianguloGrandeLado2) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLado2 = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLado1;
+
+        const trianguloPequenoLado2Cuadrado = trianguloPequenoLado2 * trianguloPequenoLado2;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase; 
+
+        const trianguloPequenoLado1 = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLado2Cuadrado)
+
+        const triaguloGrandeAltura = trianguloPequenoLado1;
+        return triaguloGrandeAltura
+    }
+}
+
+
+// Aquí interactuamos con el HTML//
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value
+
+    const area = perimetroCuadrado(value);
+    alert(area);
+}
